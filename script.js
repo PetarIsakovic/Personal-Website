e3400252e320f16c2d1cac3febb4d4505076a5bb
Petar Isakovic <isakp7825@wrdsb.ca>
2023-07-25 22:52:19 -0400

    at lot of features added to main/home page

---
+++ script.js
@@ -0,0 +1,78 @@
+const navBar = document.getElementById("navBar");
+const mainPage = document.getElementById("mainPage");
+const nameP = document.getElementById("nameP");
+const nameE = document.getElementById("nameE");
+const nameT = document.getElementById("nameT");
+const nameA = document.getElementById("nameA");
+const nameR = document.getElementById("nameR");
+
+const lastnameI = document.getElementById("lastnameI");
+const lastnameS = document.getElementById("lastnameS");
+const lastnameA = document.getElementById("lastnameA");
+const lastnameK = document.getElementById("lastnameK");
+const lastnameO = document.getElementById("lastnameO");
+const lastnameV = document.getElementById("lastnameV");
+const lastnameI2 = document.getElementById("lastnameI2");
+const lastnameC = document.getElementById("lastnameC");
+
+
+
+navBar.style.transform = "translateY(0px)";
+navBar.style.opacity = 1;
+
+mainPage.style.transform = "translateY(0px)";
+mainPage.style.opacity = 1;
+
+nameP.style.transform = "translateY(0px)";
+nameE.style.transform = "translateY(0px)";
+nameT.style.transform = "translateY(0px)";
+nameA.style.transform = "translateY(0px)";
+nameR.style.transform = "translateY(0px)";
+
+nameP.style.opacity = 1;
+nameE.style.opacity = 1;
+nameT.style.opacity = 1;
+nameA.style.opacity = 1;
+nameR.style.opacity = 1;
+
+lastnameI.style.transform = "translateY(0px)";
+lastnameS.style.transform = "translateY(0px)";
+lastnameA.style.transform = "translateY(0px)";
+lastnameK.style.transform = "translateY(0px)";
+lastnameO.style.transform = "translateY(0px)";
+lastnameV.style.transform = "translateY(0px)";
+lastnameI2.style.transform = "translateY(0px)";
+lastnameC.style.transform = "translateY(0px)";
+
+lastnameI.style.opacity = 1;
+lastnameS.style.opacity = 1;
+lastnameA.style.opacity = 1;
+lastnameK.style.opacity = 1;
+lastnameO.style.opacity = 1;
+lastnameV.style.opacity = 1;
+lastnameI2.style.opacity = 1;
+lastnameC.style.opacity = 1;
+
+
+
+
+setTimeout(() => {
+    nameP.style.transition = '100ms';
+    nameE.style.transition = '100ms';
+    nameT.style.transition = '100ms';
+    nameA.style.transition = '100ms';
+    nameR.style.transition = '100ms';
+    console.log("wioosoasd")
+      }, 2000); // 2000 milliseconds (2 seconds) delay
+
+setTimeout(() => {
+lastnameI.style.transition = '100ms';
+lastnameS.style.transition = '100ms';
+lastnameA.style.transition = '100ms';
+lastnameK.style.transition = '100ms';
+lastnameO.style.transition = '100ms';
+lastnameV.style.transition = '100ms';
+lastnameI2.style.transition = '100ms';
+lastnameC.style.transition = '100ms';
+console.log("wioosoasd")
+    }, 2600); // 2000 milliseconds (2 seconds) delay
